a698d7ffd15684f97180d24a298ebb8cb6efe929
Lilian Rodrigues <lilianxxx@gmail.com>
2026-01-17 20:35:37 +0100

    Update tailwind.config.js with theme extensions
    Added custom theme colors and breakpoints to Tailwind config.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0d4b7c',
+        mutedGray: '#595959',
+        lightBorder: '#a6a6a6',
+        contrastHover: '#ededed',
+      },
+
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1061px", 
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ]
+};
